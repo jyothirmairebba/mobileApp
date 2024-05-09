d059bcb0fdec8dda040a23f077d33523afa9166c
dev global <skooncode@gmail.com>
2024-05-09 18:15:48 +0530

    new file changes

---
+++ app.js
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <h2> Java script main page </h2>
+
+    <p id=main"> Javascript changes </p>
+    <button type="button" onclick='document.getElementById("main").innerHtml = "Hey User Welcome"'> submit </button>
+  </body>
+</html>
